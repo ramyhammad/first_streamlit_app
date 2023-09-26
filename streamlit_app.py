4302a0886e60ea3859a605e44246199506af88c4
ramyhammad <46058680+ramyhammad@users.noreply.github.com>
2023-09-26 16:51:38 -0400

    Update streamlit_app.py
    emjoi

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 streamlit.title("My Parents New Healthy Diner")
-streamlit.header("Breakfast Menu")
+streamlit.header("🥣 Breakfast Menu 🥣")
 streamlit.text("Omega 3 & Blueberry Oatmeal")
-streamlit.text("Kale, Spinach & Rocket Smoothie")
-streamlit.text("Hard-Boiled Free-Range Egg")
+streamlit.text(" 🥗 Kale, Spinach & Rocket Smoothie  🥗")
+streamlit.text(" 🐔 Hard-Boiled Free-Range Egg 🐔")
+streamlit.text(" 🍞🥑🍞 Avocado Toast")
+  
